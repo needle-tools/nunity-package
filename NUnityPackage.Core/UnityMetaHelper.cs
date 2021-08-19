e5fce174553b0afde4092c017d366d663db52f79
Marcel Wiessler <marcel@gaisterhand.de>
2021-08-19 22:51:57 +0200

    meta

---
+++ NUnityPackage.Core/UnityMetaHelper.cs
@@ -0,0 +1,45 @@
+﻿namespace NUnityPackage.Core
+{
+	public class UnityMetaHelper
+  {
+    public const string pluginDllMetaTemplate = @"fileFormatVersion: 2
+guid: <guid>
+PluginImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  iconMap: {}
+  executionOrder: {}
+  defineConstraints: []
+  isPreloaded: 0
+  isOverridable: 1
+  isExplicitlyReferenced: 0
+  validateReferences: 1
+  platformData:
+  - first:
+      : 
+    second:
+      enabled: 0
+      settings: {}
+  - first:
+      Any: 
+    second:
+      enabled: 1
+      settings: {}
+  - first:
+      Editor: Editor
+    second:
+      enabled: 0
+      settings:
+        DefaultValueInitialized: true
+  - first:
+      Windows Store Apps: WindowsStoreApps
+    second:
+      enabled: 0
+      settings:
+        CPU: AnyCPU
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
+";
+  }
+}
